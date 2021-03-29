b4354717ce5a18e43273e7fb846bb5cb8e20bbb0
Eugene Kirpichov <ekirpichov@gmail.com>
2021-03-29 16:04:25 -0700

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env sh
 
-# abort on errors
+# Per https://cli.vuejs.org/guide/deployment.html#github-pages
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'climatefellows.org' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
-
 git push -f git@github.com:workonclimate/climatefellows.git master:gh-pages
 
 cd -

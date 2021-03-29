ab61339054207a736582df0fcb6e0dfc1e189f40
Eugene Kirpichov <ekirpichov@gmail.com>
2021-03-28 22:42:43 -0700

    remove sticky

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import Sticky from "vue-sticky-directive";
 
 Vue.config.productionTip = false;
-Vue.use(Sticky);
 
 new Vue({
   vuetify,
